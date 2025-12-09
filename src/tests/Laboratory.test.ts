c106ebb4f8638c556af72ba648218ef692986926
Théo Gillet <gillettheo1@gmail.com>
2025-12-09 10:10:04 +0100

    :white_check_mark: Add test for unknown substance

--- src/tests/Laboratory.test.ts
+++ src/tests/Laboratory.test.ts
@@ -11,4 +11,15 @@ describe('Classe Laboratory', () => {
     const quantity = laboratory.getQuantity("eau distillée");
     expect(quantity).toBe(0);
   })
+
+  test("Obtenir la quantité d'une substance inconnue", () => {
+    const laboratory = new Laboratory([
+      "eau distillée",
+      "alcool éthylique",
+      "acide sulfurique"
+    ]);
+    expect(() => {
+      laboratory.getQuantity("potion magique");
+    }).toThrow("Substance inconnue : potion magique");
+  })
 })
