8e7bf443ce1fdedc241151865837209a1b2e6a76
Théo Gillet <gillettheo1@gmail.com>
2025-12-09 11:29:10 +0100

    :adhesive_bandage: Fix quantity parameter

--- src/Laboratory/Laboratory.ts
+++ src/Laboratory/Laboratory.ts
@@ -15,8 +15,18 @@ export class Laboratory {
     return this.substancesQuantities.get(substance) || 0;
   }
 
-  public add(substance: string, quantity: number): void {
-    const normalize = substance.trim().toLowerCase();
+  public add(substance: string): void {
+    const parts = substance.trim().split(" ");
+    let quantity = parseInt(parts[0]);
+    let name: string;
+
+    if (isNaN(quantity)) {
+      throw new Error(`Quantité invalide dans : ${substance}`);
+    } else {
+      name = parts.slice(1).join(" ").trim().toLowerCase();
+    }
+
+    const normalize = name.trim().toLowerCase();
     const isKnown = this.knownSubstances.includes(normalize);
     if(!isKnown) {
       this.substancesQuantities.set(normalize, quantity);
